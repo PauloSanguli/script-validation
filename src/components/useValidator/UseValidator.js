f6dd70ac47d9e4f3bd8609a68537961fd1aa0aea
Paulo Sanguli <paulosanguli@gmail.com>
2023-12-13 21:53:20 +0000

    refatoring cod

--- src/components/useValidator/UseValidator.js
+++ src/components/useValidator/UseValidator.js
@@ -3,16 +3,25 @@ import ValidateText from './Event'
 import { useState } from 'react'
 
 
-export default function UseValidator({ typeInput, placeholderInput }){
+export default function UseValidator({ typeInput, placeholderInput, response, buttonRef }){
     const [style, setStyle] = useState("fieldMessage")
 
     var changeStyle = (e) => {
-        const response = ValidateText(e)
-        if(!response){
+        const response_filter = ValidateText(e)
+        response(response_filter)
+        
+        if(!response_filter){
             setStyle("styled")
+            alterButton(0.6)
         }else{
             setStyle("fieldMessage")
+            alterButton(1)
         }
+
+    }
+
+    var alterButton = (opacity_value) => {
+        buttonRef.current.style.opacity = opacity_value
     }
 
     return (
